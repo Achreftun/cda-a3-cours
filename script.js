95ad21169c3f19b5592e4cb3e787eeb272aa586a
unknown <Administrateur@L05.m2iformation.local>
2023-10-26 16:29:19 +0200

    ajout slider

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
 function afficherMessage() {
     alert('hello')
 }
+
+var intro = window.document.getElementById('intro')
+// console.log(intro);
+// console.log(intro.innerHTML);
+// console.log(intro.textContent);
+
+var rouges = document.getElementsByClassName('rouge')
+// for (const rouge of rouges) {
+//     console.log(rouge.innerHTML);
+// }
+var paragraphes = document.getElementsByTagName('p')
+// for (const para of paragraphes) {
+//     console.log(para.innerHTML);
+// }
+
+var notFirstParagraphes = document.querySelectorAll('p:not(:first-child)')
+// for (const para of notFirstParagraphes) {
+//     console.log(para.innerHTML);
+// }
+
+var notFirstParagraphe = document.querySelector('p:not(:first-child)')
+// console.log(notFirstParagraphe.innerHTML);
+
+
+function switchLink() {
+    var lien = document.querySelector('a')
+    // console.log(lien.getAttribute('href'));
+    lien.href = 'http://www.franceinfo.fr'
+    //lien.setAttribute('href', 'http://www.franceinfo.fr')
+    lien.innerHTML = "consulter l'actualité"
+}
